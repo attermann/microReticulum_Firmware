3d38e2593c4f56011a82bac0202162cc3660dc38
Chad Attermann <attermann@gmail.com>
2026-06-22 09:54:43 -0600

    Provisioning transfer optimizations
    Implemented provisioning transfer optimizations to compliment those
    added to microReticulum to significantly lower bandwidth/airtime while
    remotely managing a node over LoRa.
    Optimizations include schema caching in browser local storage and schema
    and data compression.

--- RNode_Firmware.ino
+++ RNode_Firmware.ino
@@ -829,9 +829,13 @@ void setup() {
     filesystem.format();
 #endif
 #if 1
-    printf("Listing filesystem /:\n");
+    TRACE("Listing filesystem /:");
     filesystem.listDirectory("/", [&](const char* path) -> void {
-      printf("  %s\n", path);
+      TRACEF("  %s", path);
+    });
+    TRACE("Listing filesystem /config/:");
+    filesystem.listDirectory("/config/", [&](const char* path) -> void {
+      TRACEF("  %s", path);
     });
 #endif
 #endif // !NDEBUG && RNS_USE_FS
